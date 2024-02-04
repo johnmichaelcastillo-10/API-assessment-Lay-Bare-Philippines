9409948949eea12ccbcd2cc37f46cbc9b93343ef
johnmichaelcastilli-10 <johnmichaelcastillo.it@gmail.com>
2024-02-04 23:02:42 +0800

    Auth done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|unique:users,username',
+            'firstName' => 'required|string',
+            'middleName' => 'required|string',
+            'lastName' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status_code' => 400,
+                'message' => 'Validation error occurred',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $register = new User();
+        $register->username = $request->input('username');
+        $register->firstName = $request->input('firstName');
+        $register->middleName = $request->input('middleName');
+        $register->lastName = $request->input('lastName');
+        $register->email = $request->input('email');
+        $register->password = Hash::make($request->input('password'));
+
+        $register->save();
+
+        $token = $register->createToken('registerToken')->plainTextToken;
+
+
+
+        return response()->json([
+            'status_code' => 201,
+            'message' => 'Registered successfully',
+            'data' => $register,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check username
+        $user = User::where('username', $fields['username'])->first();
+
+        // Check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad Credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('loginToken')->plainTextToken;
+
+        return response()->json([
+            'status_code' => 201,
+            'message' => 'Login successfully',
+            'data' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoriesController;
@@ -17,11 +18,9 @@ use App\Http\Controllers\CategoriesController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::get('users/', [UserController::class, 'index']);
+
+// Route::get('users/', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{user}', [UserController::class, 'update']);
 Route::delete('/users/{user}', [UserController::class, 'destroy']);
@@ -32,3 +31,12 @@ Route::put('/categories/{category}', [CategoriesController::class, 'update']);
 Route::delete('/categories/{category}', [CategoriesController::class, 'destroy']);
 
 Route::get('/products', [ProductController::class, 'index']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('users/', [UserController::class, 'index']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
